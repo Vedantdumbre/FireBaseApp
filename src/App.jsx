c591246d87d817aecee778aba8c0a085089f2b2d
Vedant <vedantdumbre0706@gmail.com>
2024-12-25 01:07:22 +0530

    updated more info in firebase app and fetched succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import {useState } from "react";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { AiFillPlusCircle } from "react-icons/ai";
-import {collection,getDocs} from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoMdTrash } from "react-icons/io";
+import { RiEditCircleLine } from "react-icons/ri";
 const App = () => {
-
-  const [contact,setContact] = useState([]);
+  const [contact, setContact] = useState([]);
 
   useEffect(() => {
-    const getContact = async() =>{
+    const getContact = async () => {
       try {
-        const contactRef = collection(db,"contact") ;
+        const contactRef = collection(db, "contact");
         const contactSnapshot = await getDocs(contactRef);
-        console.log(contactSnapshot);
-
+        const contactLists = contactSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContact(contactLists);
       } catch (error) {
         console.log(error);
       }
-      
     };
 
     getContact();
-  },[]);
+  }, []);
 
   return (
     <div className="max-w-[370px] mx-auto px-4">
       <Navbar />
-      <div className="flex gap-2" >
+      <div className="flex gap-2">
         <div className="flex relative items-center flex-grow">
           <FiSearch className="text-white text-2xl absolute ml-1" />
           <input
@@ -36,9 +42,25 @@ const App = () => {
             className="flex-grow h-10 bg-transparent text-white pl-8 border border-white rounded-md"
           />
         </div>
-       
-          <AiFillPlusCircle className="text-5xl text-white cursor-pointer" />
-       
+
+        <AiFillPlusCircle className="text-5xl text-white cursor-pointer" />
+      </div>
+      <div className="mt-4 gap-4 flex flex-col" >
+        {contact.map((contact) => (
+          <div key={contact.id} className="bg-yellow flex items-center justify-around rounded-lg p-2 ">
+            <div className="flex items-center justify-between gap-1 " >
+              <HiOutlineUserCircle className="text-orange text-4xl" />
+              <div className="">
+                <h1 className="font-medium">{contact.Name}</h1>
+                <p className="text-sm">{contact.email}</p>
+              </div>
+            </div>
+            <div className="flex text-3xl" >
+              <IoMdTrash className="text-orange" />
+              <RiEditCircleLine />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
