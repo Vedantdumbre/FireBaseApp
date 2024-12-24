56e6e888072e0118842b66ea432b851f6c736b4a
Vedant <vedantdumbre0706@gmail.com>
2024-12-24 21:29:35 +0530

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
 const App = () => {
   return (
-    <>
-    <Navbar/>
-    </>
-  )
-}
+    <div className="max-w-[370px] mx-auto px-4">
+      <Navbar />
+      <div className="flex gap-2" >
+        <div className="flex relative items-center flex-grow">
+          <FiSearch className="text-white text-2xl absolute ml-1" />
+          <input
+            type="text"
+            className="flex-grow h-10 bg-transparent text-white pl-8 border border-white rounded-md"
+          />
+        </div>
+       
+          <AiFillPlusCircle className="text-5xl text-white cursor-pointer" />
+       
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className='my-4 h-[80px] bg-white rounded-lg gap-2 font-medium text-xl flex items-center justify-center text-center'>
+      <img src="public/firebase.svg" alt="logo" className='h-10 mr-2' /> 
+      <h1 className='text-center'>FireBase Contact App</h1>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
